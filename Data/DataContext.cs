c3c3a86af4e5f7af795a2f02651a61517ddc45c1
Breno Fernando Saldanha Santos Carneiro <breno.carneiro@gmail.com>
2023-01-13 16:31:44 -0300

    Iniciando o teste de tecnologia

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentApi.Models;
+
+namespace PaymentApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Venda> Vendas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(connectionString: "DataSource=vendas.db;Cache=Shared");
+    }
+}
---
+++ Models/Venda.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentApi.Models
+{
+    public class Venda
+    {
+        [Key]
+        public int IdVenda { get; set; }
+        public int IdVendedor { get; set;}
+        public string NomeVendedor { get; set;}
+        [MaxLength(11)]
+        public int CpfVendedor { get; set;}
+        public string EmailVendedor { get; set; }
+        [MaxLength(11)]
+        public int TelefoneVendedor { get; set; }
+        [Required]
+        public string Produto { get; set; }
+        public enum Status
+        {
+            [Description("Aguardando Pagamento")]
+            AguardandoPagamento,
+            [Description("Cancelada")]
+            Cancelada,
+            [Description("Pagamento Aprovado")]
+            PagamentoAprovado,
+            [Description("Enviado Para Transportadora")]
+            EnviadoParaTransportadora,
+            [Description("Entregue")]
+            Entregue
+        }
+    }
+}
